8924a85635011d0ec71b61d59d979630c6da5dcc
Robbie Clutton <robert.clutton@gmail.com>
2014-02-07 22:30:44 -0500

    Use newer version of rendering models

--- src/main/scala/com/robb1e/helloworld/HelloWorldController.scala
+++ src/main/scala/com/robb1e/helloworld/HelloWorldController.scala
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation._
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.beans.factory.annotation.Autowired
 import collection.JavaConversions._
+import org.springframework.ui.Model
 
 @Controller
 @RequestMapping(Array("/"))
 class HelloWorldController @Autowired() (helloWorldService: HelloWorldName) {
 
   @RequestMapping(method = Array(RequestMethod.GET))
-  def index = {
-    val params = Map("name" -> helloWorldService.name)
-    new ModelAndView("index", params)
+  def index (model: Model) = {
+    model.addAttribute("name", helloWorldService.name)
+    "index"
   }
 
 }
